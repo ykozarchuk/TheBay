3c7a4ec76d3ce5de3f565e325abfd805503f1ff6
ykozarchuk <yevgeniy_kozarchuk@epam.com>
2022-11-30 23:12:11 +0200

    Add 3rd level redirects & categories including

--- redirects/GenerateRedirects.js
+++ redirects/GenerateRedirects.js
@@ -11,11 +11,19 @@ var XMLStreamWriter = require('dw/io/XMLStreamWriter');
 var DIRECTORY_PATH = 'src/instance/redirect-urls';
 var FILE_NAME = 'redirect-urls.xml';
 var SITES_DIRECTORY = 'sites';
+var REFINEMENT_STYLE = 'refinementStyle';
 
 exports.generate = function(args, stepExecution) {
-    var catalogID = stepExecution.getParameterValue('Catalog-ID');
+    var catalogID = stepExecution.getParameterValue('Catalog-ID') || '';
+    var hostName = stepExecution.getParameterValue('Hostname') || '';
     var refinements = (stepExecution.getParameterValue('Refinements') || '').split(',').map(function (refinement) {
         return refinement.trim();
+    });
+    var secondLevelCategories = (stepExecution.getParameterValue('Second-Level-Categories') || '').split(',').map(function (secondLevelCat) {
+        return secondLevelCat.trim();
+    });
+    var thirdLevelCategories = (stepExecution.getParameterValue('Third-Level-Categories') || '').split(',').map(function (thirdLevelCat) {
+        return thirdLevelCat.trim();
     });
 
     var directory = new File(File.IMPEX + File.SEPARATOR + DIRECTORY_PATH);
@@ -45,55 +53,106 @@ exports.generate = function(args, stepExecution) {
         var secondLevelCategoriesIterator = topLevelCategory.getOnlineSubCategories().iterator();
         var topLevelCategoryRefinements = {};
 
-        var topLevelProductSearch = new ProductSearchModel();
-        topLevelProductSearch.setCategoryID(topLevelCategory.getID());
-        topLevelProductSearch.search();
-
+        var topLevelProductSearch = getProductSearch(topLevelCategory.getID());
         var topLevelRefinements = topLevelProductSearch.refinements;
         var topLevelRefinementDefinitionsIterator = topLevelRefinements && topLevelRefinements.refinementDefinitions.iterator();
 
         while (topLevelRefinementDefinitionsIterator.hasNext()) {
             var topLevelDefinition = topLevelRefinementDefinitionsIterator.next();
-
-            if (refinements.indexOf(topLevelDefinition.getAttributeID()) === -1) {
-                continue;
-            }
-
             var topLevelRefinementValues = topLevelRefinements.getAllRefinementValues(topLevelDefinition);
-            topLevelCategoryRefinements[topLevelDefinition.getAttributeID()] = topLevelRefinementValues.toArray().map(function (topLevelRefinementValue) {
-                return topLevelRefinementValue.getDisplayValue();
-            });
+
+            if (refinements.indexOf(topLevelDefinition.getAttributeID()) > -1) {
+                topLevelCategoryRefinements[topLevelDefinition.getAttributeID()] = topLevelRefinementValues.toArray().map(function (topLevelRefinementValue) {
+                    return topLevelRefinementValue.getDisplayValue();
+                });
+            }
         }
 
         while (secondLevelCategoriesIterator.hasNext()) {
             var secondLevelCategory = secondLevelCategoriesIterator.next();
+            var thirdLevelCategoriesIterator = secondLevelCategory.getOnlineSubCategories().iterator();
+            var secondLevelCategoryRefinements = {};
 
-            var secondLevelProductSearch = new ProductSearchModel();
-            secondLevelProductSearch.setCategoryID(secondLevelCategory.getID());
-            secondLevelProductSearch.search();
-
+            var secondLevelProductSearch = getProductSearch(secondLevelCategory.getID());
             var secondLevelRefinements = secondLevelProductSearch.refinements;
             var secondLevelRefinementDefinitionsIterator = secondLevelRefinements && secondLevelRefinements.refinementDefinitions.iterator();
 
             while (secondLevelRefinementDefinitionsIterator.hasNext()) {
                 var secondLevelDefinition = secondLevelRefinementDefinitionsIterator.next();
                 var secondLevelRefinementValues = secondLevelRefinements.getAllRefinementValues(secondLevelDefinition);
-                var topLevelCategoryRefinementValues = topLevelCategoryRefinements[secondLevelDefinition.getAttributeID()];
 
-                if (!topLevelCategoryRefinementValues || topLevelCategoryRefinementValues.length <= 0) {
+                if (refinements.indexOf(secondLevelDefinition.getAttributeID()) > -1) {
+                    secondLevelCategoryRefinements[secondLevelDefinition.getAttributeID()] = secondLevelRefinementValues.toArray().map(function (secondLevelRefinementValue) {
+                        return secondLevelRefinementValue.getDisplayValue();
+                    });
+                }
+
+                if (secondLevelDefinition.getAttributeID() === REFINEMENT_STYLE
+                    || secondLevelCategories.indexOf(secondLevelCategory.getID()) === -1) {
+                    continue;
+                }
+
+                var topLevelCategoryRefinementValues = topLevelCategoryRefinements[secondLevelDefinition.getAttributeID()];
+                if (!topLevelCategoryRefinementValues || topLevelCategoryRefinementValues.length < 1) {
                     continue;
                 }
 
                 secondLevelRefinementValues.toArray().forEach(function (secondLevelRefinementValue) {
-                    if (topLevelCategoryRefinementValues.indexOf(secondLevelRefinementValue.getDisplayValue()) === -1) {
-                        return;
+                    if (topLevelCategoryRefinementValues.indexOf(secondLevelRefinementValue.getDisplayValue()) > -1) {
+                        writeToFile(
+                            xsw,
+                            hostName,
+                            topLevelProductSearch.urlRefineAttributeValue('Search-Show', secondLevelDefinition.getAttributeID(), secondLevelRefinementValue.getValue()).toString(),
+                            secondLevelProductSearch.urlRefineAttributeValue('Search-Show', secondLevelDefinition.getAttributeID(), secondLevelRefinementValue.getValue()).toString()
+                        );
+                    }
+                });
+            }
+        }
+
+        while (thirdLevelCategoriesIterator.hasNext()) {
+            var thirdLevelCategory = thirdLevelCategoriesIterator.next();
+
+            if (thirdLevelCategories.indexOf(thirdLevelCategory.getID()) === -1) {
+                continue;
+            }
+
+            var thirdLevelProductSearch = getProductSearch(thirdLevelCategory.getID());
+            var thirdLevelRefinements = thirdLevelProductSearch.refinements;
+            var thirdLevelRefinementDefinitionsIterator = thirdLevelRefinements && thirdLevelRefinements.refinementDefinitions.iterator();
+
+            while (thirdLevelRefinementDefinitionsIterator.hasNext()) {
+                var thirdLevelDefinition = thirdLevelRefinementDefinitionsIterator.next();
+                var thirdLevelRefinementValues = thirdLevelRefinements.getAllRefinementValues(thirdLevelDefinition);
+
+                if (thirdLevelDefinition.getAttributeID() !== REFINEMENT_STYLE) {
+                    continue;
+                }
+
+                var topLevelCategoryRefinementValues1 = topLevelCategoryRefinements[thirdLevelDefinition.getAttributeID()];
+                var secondLevelCategoryRefinementValues = secondLevelCategoryRefinements[thirdLevelDefinition.getAttributeID()];
+                if ((!topLevelCategoryRefinementValues1 || topLevelCategoryRefinementValues1.length < 1) && (!secondLevelCategoryRefinementValues || secondLevelCategoryRefinementValues.length < 1)) {
+                    continue;
+                }
+
+                thirdLevelRefinementValues.toArray().forEach(function (thirdLevelRefinementValue) {
+                    if (topLevelCategoryRefinementValues1 && topLevelCategoryRefinementValues1.indexOf(thirdLevelRefinementValue.getDisplayValue()) > -1) {
+                        writeToFile(
+                            xsw,
+                            hostName,
+                            topLevelProductSearch.urlRefineAttributeValue('Search-Show', thirdLevelDefinition.getAttributeID(), thirdLevelRefinementValue.getValue()).toString(),
+                            thirdLevelProductSearch.urlRefineAttributeValue('Search-Show', thirdLevelDefinition.getAttributeID(), thirdLevelRefinementValue.getValue()).toString()
+                        );
                     }
 
-                    writeToFile(
-                        xsw,
-                        topLevelProductSearch.urlRefineAttributeValue('Search-Show', secondLevelDefinition.getAttributeID(), secondLevelRefinementValue.getValue()).relative().toString(),
-                        secondLevelProductSearch.urlRefineAttributeValue('Search-Show', secondLevelDefinition.getAttributeID(), secondLevelRefinementValue.getValue()).relative().toString()
-                    );
+                    if (secondLevelCategoryRefinementValues && secondLevelCategoryRefinementValues.indexOf(thirdLevelRefinementValue.getDisplayValue()) > -1) {
+                        writeToFile(
+                            xsw,
+                            hostName,
+                            secondLevelProductSearch.urlRefineAttributeValue('Search-Show', thirdLevelDefinition.getAttributeID(), thirdLevelRefinementValue.getValue()).toString(),
+                            thirdLevelProductSearch.urlRefineAttributeValue('Search-Show', thirdLevelDefinition.getAttributeID(), thirdLevelRefinementValue.getValue()).toString()
+                        );
+                    }
                 });
             }
         }
@@ -111,14 +170,28 @@ exports.generate = function(args, stepExecution) {
 };
 
 /**
+ * Gets Product Search Model based on the passed category id.
+ * @param {string} categoryID - Cagegory id.
+ * @returns {dw.catalog.ProductSearchModel} - Product Search Model.
+ */
+function getProductSearch(categoryID) {
+    var PSM = new ProductSearchModel();
+    PSM.setCategoryID(categoryID);
+    PSM.search();
+
+    return PSM;
+}
+
+/**
  * Takes given source and destination URLs and writes them to the current redirect-urls XML file.
  * @param {dw.io.XMLStreamWriter} xsw - An XML stream writer.
+ * @param {string} hostName - Hostname.
  * @param {string} sourceURL - A source URL.
  * @param {string} destinationURL - A destination URL.
  */
-function writeToFile(xsw, sourceURL, destinationURL) {
+function writeToFile(xsw, hostName, sourceURL, destinationURL) {
     xsw.writeStartElement('redirect-url');
-    xsw.writeAttribute('uri', sourceURL);
+    xsw.writeAttribute('uri', sourceURL.replace(hostName, ''));
 
         xsw.writeStartElement('status-code');
             xsw.writeCharacters('301');
@@ -129,7 +202,7 @@ function writeToFile(xsw, sourceURL, destinationURL) {
         xsw.writeEndElement();
 
         xsw.writeStartElement('destination-url');
-            xsw.writeCharacters(destinationURL);
+            xsw.writeCharacters(destinationURL.replace(hostName, ''));
         xsw.writeEndElement();
 
         xsw.writeStartElement('copy-source-params');
